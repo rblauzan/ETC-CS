569e84f0a0c9bc7657262c3dc0bfaf5fde71f75f
rblauzan <rblauzan@gmail.com>
2024-08-15 00:54:33 -0400

    simple

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,7 @@
-//"use client";
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Providers } from "./providers";
-//import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "@/styles/index.css";
