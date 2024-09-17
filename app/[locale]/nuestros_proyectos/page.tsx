c180fbe01837a02f8604f082cc09f286b6792b9f
rblauzan <rblauzan@gmail.com>
2024-09-17 00:37:04 -0400

    arreglos

--- app/[locale]/nuestros_proyectos/page.tsx
+++ app/[locale]/nuestros_proyectos/page.tsx
@@ -1,8 +1,9 @@
-import Gallery from "@/components/Gallery/gallerycontent"
+
 
 export default function NuestrosProyectos(){
     return (
-        <Gallery/>
+        <>
+        </>
     )
 }
     
